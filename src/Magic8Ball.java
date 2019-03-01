1a349e5b05f3fcd989141e869e67b1a9bb92f398
Jan Michael Tangonan <janmichael.tangonan.2022@student.lhschools.org>
2019-03-01 14:20:20 -0500

    Staged changes

---
+++ src/Magic8Ball.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Magic8Ball {
+
+
+    public static void main(String[] args) {
+
+        //Declaring Variables
+        Scanner reader;
+        int positive = 1;
+        int positive2 = 2;
+        int testing;
+        String question;
+
+        //Initialization phase
+        if (reader.nextInt) ;
+
+    }
+}
